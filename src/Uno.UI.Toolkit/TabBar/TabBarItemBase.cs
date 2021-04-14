dc38255974984d370d8837e7361f9df0ba1598cd
Steve Bilogan <steve.bilogan@gmail.com>
2021-04-14 13:16:08 -0400

    sad

--- src/Uno.UI.Toolkit/TabBar/TabBarItemBase.cs
+++ src/Uno.UI.Toolkit/TabBar/TabBarItemBase.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows.Input;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 
 namespace Uno.UI.Toolkit
 {
-	public abstract partial class TabBarItemBase
+	public abstract partial class TabBarItemBase : ListViewItem
 	{ 
 	}
 }
