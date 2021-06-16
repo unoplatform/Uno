dccbb4d86636541f96c3a514a67fc08e17e6abfe
Steve Bilogan <steve.bilogan@gmail.com>
2021-06-16 08:49:03 -0400

    rewr

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/AppBar_g.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows.UI.Xaml.Controls
+{
+	partial class AppBar
+	{
+		public event EventHandler<object> Closed;
+		public event EventHandler<object> Closing;
+		public event EventHandler<object> Opened;
+		public event EventHandler<object> Opening;
+	}
+}
