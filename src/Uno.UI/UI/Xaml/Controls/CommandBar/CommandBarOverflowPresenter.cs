7fc3001e0a68130fc3c88bd923498bb0a808965e
Steve Bilogan <steve.bilogan@gmail.com>
2021-06-18 13:55:20 -0400

    sdfsd

--- src/Uno.UI/UI/Xaml/Controls/CommandBar/CommandBarOverflowPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/CommandBar/CommandBarOverflowPresenter.cs
@@ -1,12 +1,50 @@
-#if XAMARIN
 namespace Windows.UI.Xaml.Controls
 {
 	public  partial class CommandBarOverflowPresenter : ItemsControl
 	{
-		public CommandBarOverflowPresenter() : base()
+		private bool m_useFullWidth;
+		private bool m_shouldOpenUp;
+
+		public CommandBarOverflowPresenter()
 		{
+			m_useFullWidth = false;
+			m_shouldOpenUp = false;
+#if XAMARIN
 			ItemsPanel = new ItemsPanelTemplate(() => new StackPanel());
+#endif
+		}
+
+		public void SetDisplayModeState(bool isFullWidth, bool isOpenUp)
+		{
+			m_useFullWidth = isFullWidth;
+			m_shouldOpenUp = isOpenUp;
+
+			UpdateVisualState(false);
+		}
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			UpdateVisualState(false);
+		}
+
+		private protected override void ChangeVisualState(bool useTransitions)
+		{
+			base.ChangeVisualState(useTransitions);
+
+			if (m_useFullWidth && m_shouldOpenUp)
+			{
+				GoToState(useTransitions, "FullWidthOpenUp");
+			}
+			else if (m_useFullWidth && !m_shouldOpenUp)
+			{
+				GoToState(useTransitions, "FullWidthOpenDown");
+			}
+			else
+			{
+				GoToState(useTransitions, "DisplayModeDefault");
+			}
 		}
 	}
 }
-#endif
