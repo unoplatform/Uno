cca4d259ae45e784959427fff90295b6f3706e5d
Steve Bilogan <steve.bilogan@gmail.com>
2021-09-22 15:49:12 -0400

    asd

---
+++ src/Uno.UI/Helpers/FrameNavigationHelper.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace Uno.UI.Helpers
+{
+	public static class FrameNavigationHelper
+	{
+		public static PageStackEntry? GetCurrentEntry(Frame? frame) => frame?.CurrentEntry;
+
+		public static Page? GetInstance(PageStackEntry? entry) => entry?.Instance;
+
+		public static NavigationEventArgs CreateNavigationEventArgs(
+			object? content,
+			NavigationMode navigationMode,
+			NavigationTransitionInfo? navigationTransitionInfo,
+			object? parameter,
+			Type sourcePageType,
+			Uri? uri
+		) => new NavigationEventArgs(content, navigationMode, navigationTransitionInfo, parameter, sourcePageType, uri);
+	}
+}
